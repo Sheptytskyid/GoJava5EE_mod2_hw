3a8dc85ce19eb73b834e79fac41f7ac1f9cf4f89
Denis Azhymov <azhimov93@gmail.com>
2017-02-27 09:52:00 +0200

    Create Max_branch [#11]  Create  CRUD for skills

--- src/main/java/jdbchomework/entity/Skill.java
+++ src/main/java/jdbchomework/entity/Skill.java
@@ -2,4 +2,40 @@ package jdbchomework.entity;
 
 public class Skill {
 
+    private int id;
+    private String name;
+
+    public Skill(String name) {
+        this.name = name;
+    }
+
+    public Skill(String name, int id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
+
