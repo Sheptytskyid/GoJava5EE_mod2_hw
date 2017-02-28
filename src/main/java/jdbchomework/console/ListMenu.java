3d6a5b98d609ae666c6e4b0834ffc5ca42536a73
Denis Azhymov <azhimov93@gmail.com>
2017-02-28 14:51:36 +0200

    add new lists of menu

--- src/main/java/jdbchomework/console/ListMenu.java
+++ src/main/java/jdbchomework/console/ListMenu.java
@@ -9,6 +9,10 @@ public class ListMenu {
     private static List<String> serviceHeader = new ArrayList<>();
     private static List<String> serviceMainMenu = new ArrayList<>();
     private static List<String> serviceCompaniesMenu = new ArrayList<>();
+    private static List<String> serviceCustomersMenu = new ArrayList<>();
+    private static List<String> serviceDevelopersMenu = new ArrayList<>();
+    private static List<String> serviceProjectsMenu = new ArrayList<>();
+    private static List<String> serviceSkillsMenu = new ArrayList<>();
     private static List<String> selectionHeader = new ArrayList<>();
     private static List<String> selectionMenu = new ArrayList<>();
 
@@ -28,6 +32,26 @@ public class ListMenu {
         serviceMainMenu.add("Go to developers");
         serviceMainMenu.add("Go to projects");
         serviceMainMenu.add("Go to skills");
+        serviceCompaniesMenu.add("Get all companies");
+        serviceCompaniesMenu.add("Add new company");
+        serviceCompaniesMenu.add("Update company by ID");
+        serviceCompaniesMenu.add("Delete company by ID");
+        serviceCustomersMenu.add("Get all customers");
+        serviceCustomersMenu.add("Add new customer");
+        serviceCustomersMenu.add("Update customer by ID");
+        serviceCustomersMenu.add("Delete customer by ID");
+        serviceDevelopersMenu.add("Get all developers");
+        serviceDevelopersMenu.add("Add new developer");
+        serviceDevelopersMenu.add("Update developer by ID");
+        serviceDevelopersMenu.add("Delete developer by ID");
+        serviceProjectsMenu.add("Get all projects");
+        serviceProjectsMenu.add("Add new project");
+        serviceProjectsMenu.add("Update project by ID");
+        serviceProjectsMenu.add("Delete project by ID");
+        serviceSkillsMenu.add("Get all skills");
+        serviceSkillsMenu.add("Add new skill");
+        serviceSkillsMenu.add("Update skill by ID");
+        serviceSkillsMenu.add("Delete skill by ID");
         serviceMainMenu.add("Exit");
 
         selectionHeader.add("**************************************************************");
@@ -45,6 +69,26 @@ public class ListMenu {
 
     public static List<String> getServiceMainMenu() {
         return serviceMainMenu;
+    }
+
+    public static List<String> getServiceCompaniesMenu() {
+        return serviceCompaniesMenu;
+    }
+
+    public static List<String> getServiceCustomersMenu() {
+        return serviceCustomersMenu;
+    }
+
+    public static List<String> getServiceDevelopersMenu() {
+        return serviceDevelopersMenu;
+    }
+
+    public static List<String> getServiceProjectsMenu() {
+        return serviceProjectsMenu;
+    }
+
+    public static List<String> getServiceSkillsMenu() {
+        return serviceSkillsMenu;
     }
 
     public static List<String> getSelectionHeader() {
