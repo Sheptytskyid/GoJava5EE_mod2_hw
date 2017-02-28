b08e0d585995369bcc73bef5eada91da205972d8
Denis Azhymov <azhimov93@gmail.com>
2017-02-28 19:54:55 +0200

    refactor

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -2,38 +2,38 @@
 # USE home_work1;
 
 CREATE TABLE IF NOT EXISTS companies (
-  company_id   INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  company_id INT AUTO_INCREMENT PRIMARY KEY,
+  name       VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS customers (
-  customer_id   INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  name        VARCHAR(30) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS projects (
-  project_id          INT AUTO_INCREMENT PRIMARY KEY,
+  project_id  INT AUTO_INCREMENT PRIMARY KEY,
   name        VARCHAR(60)  NOT NULL,
   cost        INT UNSIGNED NOT NULL,
-  company_id  INT          NOT NULL,
-  customer_id INT          NOT NULL,
-  FOREIGN KEY (company_id) REFERENCES companies (company_id) ,
+  company_id  INT DEFAULT NULL,
+  customer_id INT DEFAULT NULL,
+  FOREIGN KEY (company_id) REFERENCES companies (company_id),
   FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS developers (
-  developer_id         INT AUTO_INCREMENT PRIMARY KEY,
-  name       VARCHAR(30) NOT NULL,
-  salary     INT UNSIGNED,
-  project_id INT,
+  developer_id INT AUTO_INCREMENT PRIMARY KEY,
+  name         VARCHAR(30) NOT NULL,
+  salary       INT UNSIGNED,
+  project_id   INT,
   FOREIGN KEY (project_id) REFERENCES projects (project_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS skills (
-  skill_id   INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  skill_id INT AUTO_INCREMENT PRIMARY KEY,
+  name     VARCHAR(30) NOT NULL
 );
 
 
