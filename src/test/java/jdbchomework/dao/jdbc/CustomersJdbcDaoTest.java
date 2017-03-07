73da74481081ef5777383403d3c9640ab2763b05
semyonov-m-y <semyonov-m-y@mail.ru>
2017-03-07 22:14:29 +0300

    Implement SonarQube integration v04

---
+++ src/test/java/jdbchomework/dao/jdbc/CustomersJdbcDaoTest.java
@@ -0,0 +1,16 @@
+package jdbchomework.dao.jdbc;
+
+import static org.junit.Assert.*;
+
+public class CustomersJdbcDaoTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+}
