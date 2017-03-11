b1efa936fe39ab4f4cd5e8173a7d360d6f8b2166
Denis Azhymov <azhimov93@gmail.com>
2017-03-11 23:23:34 +0200

    Refactor initDB

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -2,19 +2,19 @@
 #USE test123;
 
 CREATE TABLE IF NOT EXISTS companies (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name       VARCHAR(30) NOT NULL
+  id   INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS customers (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name        VARCHAR(30) NOT NULL
+  id   INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(30)                    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS projects (
-  id  INT AUTO_INCREMENT PRIMARY KEY,
-  name        VARCHAR(60)  NOT NULL,
-  cost        INT UNSIGNED NOT NULL,
+  id          INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name        VARCHAR(60)                    NOT NULL,
+  cost        INT UNSIGNED                   NOT NULL,
   company_id  INT DEFAULT NULL,
   customer_id INT DEFAULT NULL,
   FOREIGN KEY (company_id) REFERENCES companies (id),
@@ -22,22 +22,22 @@ CREATE TABLE IF NOT EXISTS projects (
 );
 
 CREATE TABLE IF NOT EXISTS developers (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name         VARCHAR(30) NOT NULL,
-  salary       INT UNSIGNED,
-  project_id   INT,
+  id         INT          AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name       VARCHAR(30)                             NOT NULL,
+  salary     INT UNSIGNED DEFAULT NULL,
+  project_id INT          DEFAULT NULL,
   FOREIGN KEY (project_id) REFERENCES projects (id)
 );
 
 CREATE TABLE IF NOT EXISTS skills (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name     VARCHAR(30) NOT NULL
+  id   INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(30)                    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS developers_skills (
-  developers_id INT NOT NULL,
+  id INT NOT NULL,
   skills_id     INT NOT NULL,
-  PRIMARY KEY (developers_id, skills_id),
-  FOREIGN KEY (developers_id) REFERENCES developers (id),
+  PRIMARY KEY (id, skills_id),
+  FOREIGN KEY (id) REFERENCES developers (id),
   FOREIGN KEY (skills_id) REFERENCES skills (id)
 );
